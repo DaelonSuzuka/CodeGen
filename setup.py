f4b65367ea9470a9313c1163b2e6b4394ab35c23
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-04-02 20:01:30 -0400

    Added setup.py to allow library installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='CodeGen',
+    version='0.1dev',
+    author='David Kincaid',
+    author_email='dlkincaid0@gmail.com',
+    packages=['codegen',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
